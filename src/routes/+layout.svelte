5cb14c2faa84d2010fb09a3be97077b39c9c3499
Nikomus <nick.wade@live.co.uk>
2023-05-02 12:54:09 +0100

    Add basic site structure

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Footer from '../Footer.svelte';
+	import NavBar from '../NavBar.svelte';
+	import '../app.css';
+</script>
+
+<div class="layout">
+	<NavBar />
+	<main>
+		<slot />
+	</main>
+	<Footer />
+</div>
+
+<style>
+	main {
+		flex-grow: 1;
+	}
+
+	.layout {
+		display: flex;
+		flex-direction: column;
+		min-height: 100%;
+	}
+</style>
